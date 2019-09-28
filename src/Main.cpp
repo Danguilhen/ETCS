598b57a501e6dfd344fe88292a767483e38e4c69
Nicolas Hagit config --global user.name Nicolas <nicolas.hassenfratz@estaca.eu>
2019-09-28 15:21:45 +0200

    test l 31

--- src/Main.cpp
+++ src/Main.cpp
@@ -28,6 +28,7 @@ class Donnees
 // test 27/09/19
 // test 27/09/19 a 13h20
 // test 27/09/19 a 13h40
+//TEST NICOLAS
 
 //Prototypes
 void affichageRectangle(double RE, RenderWindow & fenetre, string ecran, int * ecart);
