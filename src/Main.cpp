52149105d8d289f27a4035a57fa4afc5d3baa89e
root <root@localhost.localdomain>
2019-10-03 10:03:19 +0200

    added graphs

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,4 +1,4 @@
-#include <SFML/Graphics.hpp>
+/*#include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
 #include <SFML/System.hpp>
 #include <cstdlib>
@@ -1240,6 +1240,201 @@ void gestionnaireAffichage(donnees & train)
 	}
 }
 
+*/
+/*
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+using namespace std;
+
+int main()
+{
+    int n;
+    double resultat;
+    double pile, face;
+
+    srand(time(0));
+
+    RenderWindow app(VideoMode(800, 600, 32), "Pouet !");
+    app.setFramerateLimit(60);
+
+    Shape courbe = Shape::Line(0, 300, 800, 300, 1, Color(255, 0, 0));
+    // Ajoute un point par pixel sur la courbe
+    for(int i = 1; i < 79; i++)
+    {
+        courbe.AddPoint((i*10), 300, Color(255, 0, 0));
+    }
+
+    while (app.IsOpened())
+    {
+        Event event;
+
+        while (app.GetEvent(event))
+        {
+            if (event.type == Event::Closed)
+                app.close();
+        }
+        face = 0;
+        pile = 0;
+        for (int i = 0; i < 400; i++)
+        {
+            // Génère un nombre aléatoire 0 ou 1
+            n = rand() % 2;
+            if (n == 0)
+            {
+                face++;
+            }
+            else
+            {
+                pile++;
+            }
+        }
+        resultat = pile/face;
+
+        // Décale tous les points vers la gauche
+        for(int i = 0; i < 80; i++)
+        {
+            courbe.SetPointPosition(i, (i*10), courbe.GetPointPosition(i+1).y);
+        }
+
+        // Modifie la position du point 400 au coordonnées (400, partie entière de 300*resultat)
+        courbe.SetPointPosition(41, 400, ceil(300*resultat));
+
+        app.Clear();
+
+        app.Draw(courbe);
+
+        app.Display();
+    }
+    return EXIT_SUCCESS;
+}
+
+*/
+
+#include <SFML/System.hpp>
+#include <SFML/Graphics.hpp>
+#include <cstdio> // popen
+#include <cstring> // memset
+
+#include "graph/plot.hpp"
+
+class Pinger
+        : public sf::Drawable
+{
+public:
+    Pinger(const std::string& dns, const sf::Vector2i& location)
+        : dns_(dns)
+    {
+        plot_.setSize(sf::Vector2f(600, 400));
+        plot_.setTitle("Ping "+dns);
+        plot_.setFont("./font.ttf");
+        plot_.setXLabel("Number of ping");
+        plot_.setYLabel("milliseconds");
+        plot_.setBackgroundColor(sf::Color(rand()%255, rand()%255, rand()%255));
+        plot_.setTitleColor(sf::Color::Black);
+        plot_.setPosition(sf::Vector2f(600*location.x, 400*location.y));
+        sf::plot::Curve &curve = plot_.createCurve("ping", sf::Color::Red);
+        curve.setFill(rand() % 2);
+        curve.setThickness(2 + rand() % 10);
+        curve.setColor(sf::Color(rand()%255, rand()%255, rand()%255));
+        curve.setLimit(10 + rand() % 100);
+    }
+
+    void update()
+    {
+        sf::plot::Curve &curve = plot_.getCurve("ping");
+        curve.addValue(ping(dns_));
+        plot_.prepare();
+    }
+
+    void draw(sf::RenderTarget& target, sf::RenderStates states) const
+    {
+        target.draw(plot_, states);
+    }
+
+    /// launch the ping command and get the ping ms result
+    /// not safe !
+    /// better in a thread !
+    float ping(const std::string& dns)
+    {
+        std::string cmd = "ping " + dns + " -c 1";
+        FILE* file = popen(cmd.c_str(), "r");
+        if(!file) return -1.0f;
+        char buffer[1024];
+        std::memset(buffer, 0, 1024*sizeof(char));
+        if(fread(buffer, sizeof(char), 1024, file) == 0)
+        {
+            pclose(file);
+            return -1.0f;
+        }
+        std::string in(buffer);
+        size_t pos = in.find("time=");
+        size_t end_pos = in.find(" ms", pos+1);
+        if(pos == std::string::npos || end_pos == std::string::npos)
+        {
+            pclose(file);
+            return -1.0f;
+        }
+        in = in.substr(pos+5, end_pos-(pos+5));
+        pclose(file);
+        return std::atof(in.c_str());
+    }
+
+private:
+    sf::plot::Plot plot_;
+    std::string dns_;
+};
+
+
+int main()
+{
+    // initialize the srand
+    srand (time(NULL));
+
+    sf::RenderWindow window(sf::VideoMode(600*2, 400*2), "SFML plot", sf::Style::Default);
+
+    sf::Clock clock;
+    Pinger google_com("google.com", sf::Vector2i(0, 0));
+    Pinger google_fr("google.fr", sf::Vector2i(0, 1));
+    Pinger ensta_fr("ensta.fr", sf::Vector2i(1, 0));
+    Pinger bechu_org("bechu.org", sf::Vector2i(1, 1));
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if(event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        // Each 200 ms, a new random value is add to the random curve
+        if(clock.getElapsedTime().asMilliseconds() > 1000)
+        {
+            clock.restart();
+            google_com.update();
+            google_fr.update();
+            ensta_fr.update();
+            bechu_org.update();
+        }
+
+        window.clear();
+
+        window.draw(google_com);
+        window.draw(google_fr);
+        window.draw(ensta_fr);
+        window.draw(bechu_org);
+
+        window.display();
+    }
+
+    return 0;
+}
 
 
 
