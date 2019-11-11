ec4f3d904e67a0bee5a8059234f626881a909265
Nicolas Hagit config --global user.name Nicolas <nicolas.hassenfratz@estaca.eu>
2019-11-11 12:42:39 +0100

    chagement de branche

--- src/ETCS_Bord_Main.cpp
+++ src/ETCS_Bord_Main.cpp
@@ -5,14 +5,15 @@
 #include "TrackRelatedInputs.hpp"
 #include "TrainRelatedInputs.hpp"
 #include "SpeedAndDistanceMonitoring.hpp"
-#include "ETCS_Bord_Main.hpp"
 
 using namespace std;
 
-int main()
+class ETCS_Bord
 {
+	public :
 	TrackRelatedInputs TrackRI;
 	TrackRelatedInputs TrainRI;
+};
 
-	return 0;
-}
+
+
