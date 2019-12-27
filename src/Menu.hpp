c5ffe9db9d479caf051efab9f777c72d9df9eaac
Danguilhen <thomas.danguilhen@estaca.eu>
2019-12-27 18:37:18 +0100

    Organization 2

---
+++ src/Menu.hpp
@@ -0,0 +1,12 @@
+#ifndef MENU
+#define MENU
+
+//class Menu : public Fenetre
+//{
+//	protected :
+//		Data *data;
+//		RenderWindow *fenetre;
+//		void menu(vector <string> selection, vector <int>& enable, vector<Symbol> & symbol, string title);
+//};
+
+#endif
