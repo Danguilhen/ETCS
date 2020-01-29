475e8461af7e78dea64a58018465bc4021915ea7
Graparg <raphael.grapa@gmail.com>
2020-01-29 22:10:48 +0100

    Création train statique + pupitre entrant

---
+++ src/TrainStatique.cpp
@@ -0,0 +1,166 @@
+//#include "TrainStatique.hpp"
+//
+//TrainStatique::TrainStatique(PupitreEntrant &PE)
+//{
+//	this ->PE = &PE;
+//	int etat_projecteurs = 0;
+//	int etat_disj = 0;
+//	int etat_sablage = 0;
+//	bool etat_acquitt = 0;
+//	int etat_essui_glace = 0;
+//	bool etat_lave_glace = 0;
+//	int etat_light_cabine = 0;
+//	int etat_light_pupitre = 0;
+//	int etat_sifflet = 0;
+//	bool etat_LS_F = 0;
+//	bool etat_BP_SF = 0;
+//	int signalisation = 0;
+//	float etat_VI = 0;
+//	int direction = 0;
+//	int etat_conduct_mod = 0;
+//	float etat_tract_frein = 0;
+//	int etat_porte = 0;
+//	bool etat_panto = 0;
+//	bool etat_VA = 0;
+//	bool etat_cle_BL = 0;
+//	bool etat_ventilation = 0;
+//	int etat_GROG = 0;
+//	bool etat_TVM_V1 = 0;
+//	bool etat_TVM_V2 = 0;
+//	bool etat_alarme = 0;
+//}
+//
+//// Les seters
+//
+//// Voir peut-être si coder CG CP
+//// Voir les relations entre les différents états
+//// Voir comment coder différemment un bouton et un interrupteur
+//
+//void TrainStatique::set_projecteurs()
+//{etat_projecteurs = PE->getCmd_projecteurs();}
+//
+//void TrainStatique::set_disj()
+//{
+//	// Timer : maintien 2s pour l'armement uniquement
+//	// Voir Benoît pour savoir comment gérer le bouton armement et fermeture
+//}
+//
+//void TrainStatique::set_sablage()
+//{
+//	// Timer : maintien
+//
+//}
+//
+//void TrainStatique::set_acquitt()
+//{
+//	// Timer : à voir
+//}
+//
+//void TrainStatique::set_essui_glace()
+//{etat_essui_glace = PE->getCmd_essui_glace();}
+//
+//void TrainStatique::set_lave_glace()
+//{
+//	// Timer : voir
+//	etat_lave_glace = PE->getCmd_lave_glace();
+//}
+//
+//void TrainStatique::set_light_cabine()
+//{
+//	// Timer : voir
+//
+//}
+//
+//void TrainStatique::set_light_pupitre()
+//{
+//	// Timer : voir
+//
+//}
+//
+//void TrainStatique::set_sifflet()
+//{
+//	///
+//}
+//
+//void TrainStatique::set_LS_SF()
+//{
+//	// C'est la lumière qui est modifiée par l'acquittement
+//}
+//
+//void TrainStatique::set_BP_SF()
+//{
+//
+//}
+//
+//void TrainStatique::set_signalisation()
+//{
+//	// C'est écrit dans le manuel de spec comment je dois le coder
+//}
+//
+//void TrainStatique::set_VI()
+//{VI = PE->getCmd_VI();}
+//
+//void TrainStatique::set_direction()
+//{direction = PE->getCmd_inverseur();}
+//
+//void TrainStatique::set_conduct_mod()
+//{conduct_mod = PE->getCmd_conduct_mod();}
+//
+//void TrainStatique::set_tract_frein()
+//{tract_frein = PE->getCmd_tract_frein();}
+//
+//void TrainStatique::setPorte()
+//{
+//
+//}
+//
+//void TrainStatique::set_panto()
+//{
+//
+//}
+//
+//void TrainStatique::set_VA()
+//{
+//
+//}
+//
+//void TrainStatique::set_cle_BL()
+//{
+//
+//}
+//
+//void TrainStatique::set_ventilation()
+//{
+//
+//}
+//
+//void TrainStatique::set_GROG()
+//{
+
+//}
+//
+//void TrainStatique::set_TVM_V1()
+//{
+//
+//}
+//
+//void TrainStatique::set_TVM_V2()
+//{
+//
+//}
+//
+//void TrainStatique::set_alarme()
+//{
+//
+//}
+
+//int TrainStatique::getPanto(){return panto;}
+//int TrainStatique::getSifflet(){return sifflet;}
+//bool TrainStatique::getDisj(){return disj;}
+
+//void TrainStatique::update()
+//{
+//	set_disj();
+//	set_panto();
+//	set_sifflet();
+//}
---
+++ src/TrainStatique.hpp
@@ -0,0 +1,69 @@
+//#ifdef NJDSJ
+//#define NJDSJ
+//
+//#include "define.hpp"
+//#include "PupitreEntrant.hpp"
+//
+//class TrainStatique
+//{
+//	private :
+//		PupitreEntrant *PE;
+//		int etat_projecteurs;
+//		int etat_disj;
+//		int etat_sablage;
+//		bool etat_acquitt;
+//		int etat_essui_glace;
+//		bool etat_lave_glace;
+//		int etat_light_cabine;
+//		int etat_light_pupitre;
+//		int etat_sifflet;
+//		bool etat_LS_SF;
+//		bool etat_BP_SF;
+//		int signalisation;
+//		float etat_VI;
+//		int direction;
+//		int etat_conduct_mod;
+//		float etat_tract_frein;
+//		int etat_porte;
+//		bool etat_panto;
+//		bool etat_VA;
+//		bool etat_cle_BL;
+//		bool etat_ventilation;
+//		int etat_GROG;
+//		bool etat_TVM_V1;
+//		bool etat_TVM_V2;
+//		bool etat_alarme;
+//	public :
+//		TrainStatique(PupitreEntrant &PE);
+//		void set_projecteurs();
+//		void set_disj();
+//		void set_sablage();
+//		void set_acquitt();
+//		void set_essui_glace();
+//		void set_lave_glace();
+//		void set_light_cabine();
+//		void set_light_pupitre();
+//		void set_sifflet();
+//		void set_LS_SF();
+//		void set_BP_SF();
+//		void set_signalisation();
+//		void set_VI();
+//		void set_direction();
+//		void set_conduct_mod();
+//		void set_tract_frein();
+//		void setPorte();
+//		void set_panto();
+//		void set_VA();
+//		void set_cle_BL();
+//		void set_ventilation();
+//		void set_GROG();
+//		void set_TVM_V1();
+//		void set_TVM_V2();
+//		void set_alarme();
+////		int getPanto();
+////		bool getDisj();
+////		int getSifflet();
+////		void update();
+//};
+//
+//#endif
