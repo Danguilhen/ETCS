e476b80dd0a047c876562628ca89c053423aac1b
Danguilhen <thomas.danguilhen@estaca.eu>
2020-03-29 16:43:02 +0200

    Reseau Toto

--- src/Virtual_train.hpp
+++ src/Virtual_train.hpp
@@ -5,21 +5,22 @@
 #include "ETCS.hpp"
 #include "DMI/BASIC.hpp"
 #include "Train_dynamique.hpp"
+#include "TrainStatique.hpp"
 #include "Reseau.hpp"
 
 class Virtual_train
 {
-	public :
-		//Data data;
-		Reseau reseau;
-		Software soft;
-		ETCS etcs;
-		BASIC basic; // écran affichage restreint
-		BASIC auxiliaire; // écran avec affichage information auxiliaire
-		Train_dynamique train_dynamique;
-		//Train_Statique train_statique;
-		Virtual_train();
-		void update();
+    public :
+        //Data data;
+        Reseau reseau;
+        Software soft;
+        ETCS etcs;
+        BASIC basic; // écran affichage restreint
+        BASIC auxiliaire; // écran avec affichage information auxiliaire
+        Train_dynamique train_dynamique;
+        TrainStatique trainStatique;
+        Virtual_train();
+        void update();
 };
 
 #endif
